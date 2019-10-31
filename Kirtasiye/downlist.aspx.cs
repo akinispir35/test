af64d19b2ef62c5568335f1104f8398313ffbf62
akinispir35@hotmail.com <akinispir35@hotmail.com>
2019-10-31 16:24:51 +0300

    denemeee

---
+++ Kirtasiye/downlist.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace Kirtasiye
+{
+    public partial class downlist : System.Web.UI.Page
+    {
+        SqlConnection baglanti = new SqlConnection("Server=192.168.1.200;user id=akin;Password=akin;Database=AKIN;");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("Select * From Sehir",baglanti);
+
+            //SqlDataAdapter da = new SqlDataAdapter();
+            //da.SelectCommand = komut;
+            //DataTable dt = new DataTable();
+            //da.Fill(dt);
+            //DropDownList1.DataSource = dt;
+            //DropDownList1.DataBind();
+            SqlDataReader dr = komut.ExecuteReader();
+            DropDownList1.DataSource = dr;
+            DropDownList1.DataValueField = "SehirId";
+            DropDownList1.DataTextField = "SehirAd";
+            DropDownList1.DataBind();
+
+
+        }
+    }
+}
---
+++ Kirtasiye/downlist.aspx.designer.cs
@@ -0,0 +1,35 @@
+﻿//------------------------------------------------------------------------------
+// <otomatik üretildi>
+//     Bu kod bir araç tarafından oluşturuldu.
+//
+//     Bu dosyada yapılacak değişiklikler yanlış davranışa neden olabilir ve
+//     kod tekrar üretildi. 
+// </otomatik üretildi>
+//------------------------------------------------------------------------------
+
+namespace Kirtasiye
+{
+
+
+    public partial class downlist
+    {
+
+        /// <summary>
+        /// form1 denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// DropDownList1 denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+    }
+}
